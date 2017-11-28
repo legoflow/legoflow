71679cb278daca8b8927bda28fdbdeb99a595257
lijialiang <lijialiang@yy.com>
2017-11-28 09:31:12 +0800

    [update] demo-proxy

--- demo/demo-proxy/src/js/main.js
+++ demo/demo-proxy/src/js/main.js
@@ -1,3 +1,11 @@
-fetch('/mock').then(( r ) => {
-    console.log( r );
+fetch('/user').then(( r ) => {
+    console.log( 'user:', r );
+})
+
+fetch('/mock-api').then(( r ) => {
+    console.log( 'mock:', r );
+})
+
+fetch('/api/14').then(( r ) => {
+    console.log( 'api', r );
 })
